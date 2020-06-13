94f5f99b834a89421ecd4d31963be72d90a357db
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-06-13 20:07:56 +0200

    eureka config

--- src/main/java/it/frigir/msscbrewerygateway/config/LoadBalanceRoutesConfig.java
+++ src/main/java/it/frigir/msscbrewerygateway/config/LoadBalanceRoutesConfig.java
@@ -6,21 +6,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("!local-discovery")
+@Profile("local-discovery")
 @Configuration
-public class LocalHostRouteConfig {
+public class LoadBalanceRoutesConfig {
 
     @Bean
-    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
-                        .uri("localhost:8080")
+                        .uri("lb://beer-service")
                         .id("beer-service"))
                 .route(r -> r.path("/api/v1/customers/**")
-                        .uri("localhost:8081")
+                        .uri("lb://beer-order-service")
                         .id("beer-order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory")
-                        .uri("locahost:8082")
+                        .uri("lb://beer-inventory-service")
                         .id("beer-inventory-service"))
                 .build();
     }
